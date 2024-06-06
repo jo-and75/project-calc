2fe355e91cf62ac14105e5ce8a4593af97457158
jo-and75 <jaotika@gmail.com>
2024-06-06 12:20:33 +0300

    Create div for calculator and add a class for it

---
+++ main.js
@@ -0,0 +1,11 @@
+const body = document.getElementsByTagName("body");
+const calculatorBase = document.createElement("div")  
+calculatorBase.classList.add("calcBase");
+
+
+
+
+
+
+
+body.appendChild(calculatorBase);
